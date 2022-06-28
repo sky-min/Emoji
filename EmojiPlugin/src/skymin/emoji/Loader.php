6a9b4b955061287719aa1edb17abfa7e5b467860
Arie <nttislinked@gmail.com>
2022-06-28 21:14:04 +0700

    rp

--- EmojiPlugin/src/skymin/emoji/Loader.php
+++ EmojiPlugin/src/skymin/emoji/Loader.php
@@ -25,37 +25,60 @@ final class Loader extends PluginBase{
 	 */
 	public function onLoad() : void{
 		self::$instance = $this;
-		$resource = $this->getResource(self::pack_name);
-		if ($resource !== null) {
-			$fp = fopen($this->getServer()->getDataPath() . "resource_packs/" . self::pack_name, "wb");
-			if($fp === false) {
-				throw new AssumptionFailedError("fopen() should not fail with wb flags");
+		$this->saveResource("emoji.json");
+
+		$rp_path = $this->getServer()->getDataPath() . "resource_packs/";
+
+		// Copy the pack to the resource packs folder
+		if (!file_exists($rp_path)) {
+			$resource = $this->getResource(self::pack_name);
+			if ($resource !== null) {
+				$fp = fopen($rp_path . self::pack_name, "wb");
+				if($fp === false) {
+					throw new AssumptionFailedError("fopen() should not fail with wb flags");
+				}
+				stream_copy_to_stream($resource, $fp);
+				fclose($fp);
+				fclose($resource);
 			}
-			stream_copy_to_stream($resource, $fp);
-			fclose($fp);
-			fclose($resource);
 		}
 
 		$resourcepackManager = $this->getServer()->getResourcePackManager();
-
+		/*
+		$pack = new ZippedResourcePack($rp_path);
+		$uuid = $pack->getPackId();
+		$version = $pack->getPackVersion();
+		 */
 		$pack = $resourcepackManager->getPackById(self::uuid);
 		if ($pack === null) {
-			$pack = new ZippedResourcePack($this->getDataFolder() . "PenPlugin.mcpack");
+			$pack = new ZippedResourcePack($rp_path . self::pack_name);
 			$reflection = new \ReflectionClass($resourcepackManager);
 			$property = $reflection->getProperty("resourcePacks");
+			/** @var mixed $property */
+			$property->setAccessible(true);
 			$currentResourcePacks = $property->getValue($resourcepackManager);
 			$currentResourcePacks[] = $pack;
 			$property->setValue($resourcepackManager, $currentResourcePacks);
 
+
 			$property = $reflection->getProperty("uuidList");
+			$property->setAccessible(true);
 			$currentUUIDPacks = $property->getValue($resourcepackManager);
 			$currentUUIDPacks[strtolower($pack->getPackId())] = $pack;
 			$property->setValue($resourcepackManager, $currentUUIDPacks);
+
+			$data = new Data($rp_path . "resource_packs.yml"); //Add the pack to the resourcepack stack
+			$rpstack = $data->__get("resource_stack");
+			$rpstack[] = basename(self::pack_name);
+			$data->__set("resource_stack", $rpstack);
+			$data->save();
 		}
 
 		if (!$resourcepackManager->resourcePacksRequired()) {
 			$reflection = new \ReflectionClass($resourcepackManager);
 			$property = $reflection->getProperty("serverForceResources");
+			/** @var mixed $property */
+			$property->setAccessible(true);
 			$property->setValue($resourcepackManager, true);
 		}
 		if ($pack->getPackVersion() !== self::version) {
